452217c2cb2b70bed99975a7156646b6dd2afca5
tomny <20028678+tmyngu11@users.noreply.github.com>
2023-09-28 20:47:29 -0400

    disable failsafe

--- src/headless.py
+++ src/headless.py
@@ -2,6 +2,8 @@ from model.vulcan.woodcutting import VulcanWoodcutter
 from model.vulcan.mining import VulcanMiner
 from model.vulcan.thieving import VulcanThiever
 from OSBC import App
+import pyautogui
+pyautogui.FAILSAFE = False
 
 if __name__ == "__main__":
     app = App(test=True)
