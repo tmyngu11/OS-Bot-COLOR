99a1b1de0de39a1abc753253b71688296255737b
tomny <20028678+tmyngu11@users.noreply.github.com>
2023-10-12 18:14:32 -0400

    Update headless.py

--- src/headless.py
+++ src/headless.py
@@ -2,10 +2,11 @@ from model.vulcan.woodcutting import VulcanWoodcutter
 from model.vulcan.mining import VulcanMiner
 from model.vulcan.thieving import VulcanThiever
 from model.vulcan.barrows import VulcanBarrows
+from model.vulcan.superheat import VulcanSuperheat
 from OSBC import App
 import pyautogui
 pyautogui.FAILSAFE = False
 
 if __name__ == "__main__":
     app = App(test=True)
-    app.test(VulcanBarrows())
+    app.test(VulcanMiner("RuneLite"))
